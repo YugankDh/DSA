29c9c8f7d362b80ca7c2979a7ff8ba97931f706a
YugankDh <147908064+YugankDh@users.noreply.github.com>
2026-06-05 13:37:23 +0530

    star-pyramid

---
+++ patterns/star-pyramid.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+
+
+    for (int i = 0; i < n; i++)
+    {
+    	// space 
+
+    	for (int j = 0;j < n-1-i;j++)
+    	{
+    		cout<<" ";
+    	};
+    	
+    	// star
+
+    	for (int j = 0; j < 2 * i + 1; j++)
+    	{
+    		cout << "*";
+    	};
+    	
+    	//space
+
+    	for (int j = 0; j < n - 1 - i ; j++)
+    	{
+    		cout<<" ";
+    	};
+    	cout << endl;
+    };
+
+    
+
+
+
+    return 0;
+}
